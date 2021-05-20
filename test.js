0658103ebd901927928a4cac2fd33d99efc7f3f5
yu-ta38 <hyuta@outlook.jp>
2021-05-21 04:28:51 +0900

    commit

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-//js:window.onload = function(){}
-/*window.onload = function() {
-    let n = 3;
-    for(i = 0;i < n;i++) {
-        let pass = prompt("password?");
-        if(pass=="yuta38") {
-            break;
-        }
-        if(i==n-1) {
-            alert("Error:You are not admin.");
-            window.location.href = 'https://yu-ta38.github.io/MyPage/error.html';
-            window.open('https://yu-ta38.github.io/MyPage/error.html', '_blank');
-        }
-    }
-}*/
-
 function functions() {
     alert('world!');
 }
